9bda7f68e8270028c9a7e2ec348db2f392cf8697
Bruce Campbell <wavescholar@gmail.com>
2020-12-28 13:55:32 -0500

    fix

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a bookdown publication of selected topics in data science. 
---
+++ _deploy.sh
@@ -0,0 +1,9 @@
+git checkout --orphan gh-pages
+git rm -rf .
+
+# create a hidden file .nojekyll
+touch .nojekyll
+git add .nojekyll
+
+git commit -m"Initial commit"
+git push origin gh-pages
